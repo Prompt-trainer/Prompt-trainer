f2025b1ae346ef956bce49917a342057152ae489
IllyaDD <illya.d.donchenko@gmail.com>
2026-02-02 16:13:47 +0200

    fix: chat now works with https

--- prompt_train/prompt_train/asgi.py
+++ prompt_train/prompt_train/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt_train.settings")
+django.setup()  # ВАЖЛИВО: ініціалізуємо Django перед імпортами
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prompt_train.settings")
 
 django_asgi_app = get_asgi_application()
 
